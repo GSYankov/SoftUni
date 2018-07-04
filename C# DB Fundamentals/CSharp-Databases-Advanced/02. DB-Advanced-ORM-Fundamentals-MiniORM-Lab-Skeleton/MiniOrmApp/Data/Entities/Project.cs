e5b438e7ae01844112eb6316c8c56736f36a4a80
Georgi Yankov <spookyfox@abv.bg>
2018-07-04 16:47:40 +0300

    Working instance

---
+++ C# DB Fundamentals/CSharp-Databases-Advanced/02. DB-Advanced-ORM-Fundamentals-MiniORM-Lab-Skeleton/MiniOrmApp/Data/Entities/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MiniOrmApp.Data.Entities
+{
+    [Table("Projects")]
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<EmployeeProject> EmployeeProjects { get; }
+    }
+}
---
+++ C# DB Fundamentals/CSharp-Databases-Advanced/02. DB-Advanced-ORM-Fundamentals-MiniORM-Lab-Skeleton/MiniOrmApp/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using MiniOrmApp.Data;
+using MiniOrmApp.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniOrmApp
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = @"Server=DESKTOP-1KC3O05\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inerted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+
+    }
+}
