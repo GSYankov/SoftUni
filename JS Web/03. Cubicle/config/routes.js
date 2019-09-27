1ffa588a11661fffdfee2303ec836ed62418396f
Georgi Yankov <spookyfox@abv.bg>
2019-09-27 14:28:44 +0300

    Introduce full CRUD

--- JS Web/03. Cubicle/config/routes.js
+++ JS Web/03. Cubicle/config/routes.js
@@ -7,7 +7,10 @@ module.exports = app => {
     app.get('/create', cubeController.create);
     app.post('/create', cubeController.createPost);
     app.get('/details/:cubeId', cubeController.details);
-    app.post('/delete', cubeController.delete);
+    app.post('/delete', cubeController.deletePost);
+    app.get('/edit/:cubeId', cubeController.edit);
+    app.post('/edit', cubeController.editPost);
+
 
     //The 404 Route (ALWAYS Keep this as the last route)
     app.get('*', homeController.error);
--- JS Web/03. Cubicle/controllers/cube.js
+++ JS Web/03. Cubicle/controllers/cube.js
@@ -32,7 +32,7 @@ module.exports = {
 
         res.redirect('/');
     },
-    delete: (req, res) => {
+    deletePost: (req, res) => {
         let cubeId = req.body.cubeId;
 
         fs.readFile('./config/database.json', 'utf-8', (err, data) => {
@@ -52,6 +52,59 @@ module.exports = {
             });
         });
 
+
+        res.redirect('/');
+    },
+    edit: (req, res) => {
+        let cubeId = req.params.cubeId;
+        fs.readFile('./config/database.json', 'utf-8', (err, data) => {
+
+            let cube = JSON.parse(data).filter(function(cube) {
+                return cube.id === cubeId;
+            })[0];
+
+            let l1 = cube.level === "1" ? 'selected' : "";
+            let l2 = cube.level === "2" ? 'selected' : "";
+            let l3 = cube.level === "3" ? 'selected' : "";
+            let l4 = cube.level === "4" ? 'selected' : "";
+            let l5 = cube.level === "5" ? 'selected' : "";
+            let l6 = cube.level === "6" ? 'selected' : "";
+
+            res.render('cube/edit', { cube, l1, l2, l3, l4, l5, l6 });
+        });
+    },
+
+    editPost: (req, res) => {
+        let cubeId = req.body.cubeId;
+        let name = req.body.name;
+        let image = req.body.imageUrl;
+        let description = req.body.description;
+        let level = req.body.difficultyLevel;
+
+        let cube = { id: cubeId, name: name, image: image, description: description, level: level };
+
+        fs.readFile('./config/database.json', 'utf-8', (err, data) => {
+
+            let newCubesArr = JSON.parse(data);
+
+            for (var i in newCubesArr) {
+                if (newCubesArr[i].id == cubeId) {
+                    newCubesArr[i] = cube;
+                    break; //Stop this loop, we found it!
+                }
+            }
+
+
+            let newCubesJson = JSON.stringify(newCubesArr);
+
+            fs.writeFile('./config/database.json', newCubesJson, (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Cube with id: ${cubeId} has been edited successfully!`)
+            });
+        });
 
         res.redirect('/');
     }
